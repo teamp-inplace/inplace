0a683c0f1eca3fef5697657c98e1fbfb616a2fd9
sanghee0820 <lsh277604@gmail.com>
2024-12-30 13:14:28 +0900

    INPLACE-57 Influencer 등록 페이지 추가

--- backend/src/main/java/team7/inplace/admin/AdminPageController.java
+++ backend/src/main/java/team7/inplace/admin/AdminPageController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import team7.inplace.admin.banner.persistence.BannerRepository;
+import team7.inplace.global.config.YoutubeApiProperties;
 import team7.inplace.global.kakao.config.KakaoApiProperties;
 import team7.inplace.video.domain.Video;
 import team7.inplace.video.persistence.VideoRepository;
@@ -18,6 +19,7 @@ import team7.inplace.video.persistence.VideoRepository;
 @RequestMapping("/admin")
 public class AdminPageController {
     private final KakaoApiProperties kakaoApiProperties;
+    private final YoutubeApiProperties youtubeApiProperties;
     private final VideoRepository videoRepository;
     private final BannerRepository bannerRepository;
 
@@ -39,6 +41,12 @@ public class AdminPageController {
 
         model.addAttribute("banners", banners);
         return "admin/banner.html";
+    }
+
+    @GetMapping("/influencer/new")
+    public String getIncluencers(Model model) {
+        model.addAttribute("youtubeApiKey", youtubeApiProperties.key());
+        return "admin/influencer/new.html";
     }
 
     @GetMapping("/main")
