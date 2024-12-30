21b5086567dbd5bc5839ae5063f08167979e97d7
sanghee0820 <lsh277604@gmail.com>
2024-12-30 12:09:39 +0900

    INPLACE-57 Entity이름 -> 테이블 이름으로 수정

--- backend/src/main/java/team7/inplace/influencer/domain/Influencer.java
+++ backend/src/main/java/team7/inplace/influencer/domain/Influencer.java
@@ -8,7 +8,8 @@ import lombok.NoArgsConstructor;
 import static lombok.AccessLevel.PROTECTED;
 
 @Getter
-@Entity(name = "influencers")
+@Entity
+@Table(name = "influencers")
 @NoArgsConstructor(access = PROTECTED)
 public class Influencer {
     @Id
