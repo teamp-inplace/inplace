8975065d1724f2e3f15f0b46950d403db34b2149
sanghee0820 <lsh277604@gmail.com>
2025-01-01 11:20:15 +0900

    INPLACE-57 MenuBoardPhoto에 Getter 추가

--- backend/src/main/java/team7/inplace/place/domain/MenuBoardPhoto.java
+++ backend/src/main/java/team7/inplace/place/domain/MenuBoardPhoto.java
@@ -7,9 +7,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
 @Table(name = "menu_board_photos")
 @NoArgsConstructor(access = PROTECTED)
 public class MenuBoardPhoto {
