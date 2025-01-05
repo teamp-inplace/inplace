f0a8ea389db6b3347ec4422568798c320213f3f9
suhyeon7497 <nucc1186@gmail.com>
2025-01-05 15:32:44 +0900

    [INPLACE-74] placeMessageUtil to placeUtil

--- backend/src/main/java/team7/inplace/place/util/KakaoMessageMaker.java
+++ backend/src/main/java/team7/inplace/place/util/KakaoMessageMaker.java
@@ -1,4 +1,4 @@
-package team7.inplace.placeMessage.util;
+package team7.inplace.place.util;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
