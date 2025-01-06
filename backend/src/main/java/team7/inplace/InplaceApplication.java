d1ff656172d0aff913b5ce3c3fec72a1f7b855ac
BaeJunho <bjh4498@knu.ac.kr>
2025-01-06 15:37:56 +0900

    [INPLACE-72] baseEntity 사용 설정 및 구성

--- backend/src/main/java/team7/inplace/InplaceApplication.java
+++ backend/src/main/java/team7/inplace/InplaceApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import team7.inplace.token.persistence.RefreshTokenRepository;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableJpaAuditing
 @EnableJpaRepositories(
         basePackages = "team7.inplace",
         excludeFilters = @ComponentScan.Filter(
