33ad1905c7d28498b001d5b45c06de9febc3608f
suhyeon7497 <nucc1186@gmail.com>
2025-01-07 18:28:48 +0900

    [INPLACE-74] merge conflict resolve

--- backend/src/main/java/team7/inplace/place/util/FeedTemplate.java
+++ backend/src/main/java/team7/inplace/place/util/FeedTemplate.java
@@ -14,13 +14,14 @@ public record FeedTemplate(
     private final static String BUTTON_NAME = "리뷰 남기기";
     private final static String OBJECT_TYPE = "feed";
 
-    public static FeedTemplate of(String frontEndUrl, PlaceMessageCommand placeMessageCommand) {
-        Link link = Link.of(frontEndUrl + "/places/" + placeMessageCommand.placeId() + "/reviews");
+    public static FeedTemplate of(String frontEndUrl, PlaceMessageCommand placeMessageCommand, String uuid) {
+        Link link = Link.of(
+            frontEndUrl + "/places/" + placeMessageCommand.placeId() + "/reviews/" + uuid);
         return new FeedTemplate(
-                OBJECT_TYPE,
-                Content.of(placeMessageCommand, "함께 해주셔서 감사합니다! 리뷰 부탁드려요.",
-                        link),
-                List.of(Button.of(BUTTON_NAME, link))
+            OBJECT_TYPE,
+            Content.of(placeMessageCommand, "함께 해주셔서 감사합니다! 리뷰 부탁드려요.",
+                link),
+            List.of(Button.of(BUTTON_NAME, link))
         );
     }
 }
