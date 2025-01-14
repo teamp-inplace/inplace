fde1804e91a22f028d3af47575da9559717d3793
sanghee0820 <lsh277604@gmail.com>
2025-01-14 23:18:40 +0900

    INPLACE-87 Redis 시작 시 Config명령어를 사용하지 않도록 변경

--- backend/src/main/java/team7/inplace/global/redis/RedisConfig.java
+++ backend/src/main/java/team7/inplace/global/redis/RedisConfig.java
@@ -6,13 +6,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisKeyValueAdapter;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-@EnableRedisRepositories(enableKeyspaceEvents = RedisKeyValueAdapter.EnableKeyspaceEvents.ON_STARTUP)
 @Configuration
 public class RedisConfig {
 
