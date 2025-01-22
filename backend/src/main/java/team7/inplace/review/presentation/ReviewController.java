ea8cf12076a91b97873b443dab70b4d86e337b4f
sanghee0820 <lsh277604@gmail.com>
2025-01-22 13:25:29 +0900

    INPLACE-104 리뷰 UUID 정보 조회 API 추가

--- backend/src/main/java/team7/inplace/review/presentation/ReviewController.java
+++ backend/src/main/java/team7/inplace/review/presentation/ReviewController.java
@@ -4,24 +4,38 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import team7.inplace.review.application.ReviewInvitationService;
 import team7.inplace.review.application.ReviewService;
 import team7.inplace.review.presentation.dto.ReviewRequest;
+import team7.inplace.review.presentation.dto.ReviewResponse;
+import team7.inplace.review.presentation.dto.ReviewResponse.Invitation;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/reviews")
 public class ReviewController implements ReviewControllerApiSpec {
 
+    private final ReviewInvitationService reviewInvitationService;
     private final ReviewService reviewService;
     private final ReviewInvitationService reviewInvitationService;
 
-    //    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @Override
+    @GetMapping
+    public ResponseEntity<Invitation> getReviewInfo(@RequestParam String uuid) {
+        var invitationInfo = reviewInvitationService.getReviewInvitation(uuid);
+
+        var response = ReviewResponse.Invitation.from(invitationInfo);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @Override
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteReview(@PathVariable Long id) {
         reviewService.deleteReview(id);
--- backend/src/main/java/team7/inplace/review/presentation/ReviewControllerApiSpec.java
+++ backend/src/main/java/team7/inplace/review/presentation/ReviewControllerApiSpec.java
@@ -1,12 +1,23 @@
 package team7.inplace.review.presentation;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import team7.inplace.review.presentation.dto.ReviewResponse;
 import team7.inplace.review.presentation.dto.ReviewRequest;
 
+@Tag(name = "리뷰", description = "리뷰 관련 API")
 public interface ReviewControllerApiSpec {
+
+    @Operation(summary = "리뷰의 정보를 조회합니다.", description = "UUID를 바탕으로 장소정보, 인플루언서 이름, 유저 닉네임을 반환합니다.")
+    ResponseEntity<ReviewResponse.Invitation> getReviewInfo(
+        @Parameter(description = "리뷰 UUID", required = true) @RequestParam String uuid
+    );
+
 
     @Operation(summary = "리뷰 삭제", description = "본인이 작성한 리뷰를 삭제합니다.")
     ResponseEntity<Void> deleteReview(@PathVariable Long id);
