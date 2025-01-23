059955918482bcca7bc40a37704bc1ef1c8e0d1b
Hyoeunkh <lhe4528@knu.ac.kr>
2025-01-23 23:12:33 +0900

    [INPLACE-17] 배포용 서버에서 작동되는지 확인하기 위해 임시 설정

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -18,12 +18,17 @@ async function startApp() {
 
 Sentry.init({
   dsn: import.meta.env.VITE_SENTRY_DSN,
-  enabled: import.meta.env.PROD,
+  enabled: true,
   integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
   // Tracing
   tracesSampleRate: import.meta.env.PROD ? 0.1 : 0,
   // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: ['localhost', /^https:\/\/api.inplace.my/],
+  tracePropagationTargets: [
+    /^https:\/\/a7b2c3d4-dev\.inplace\.my/, // 개발 서버 도메인
+    /^https:\/\/api\.inplace\.my/, // 배포 서버 도메인
+    '!http://localhost', // localhost는 제외
+    '!https://localhost',
+  ],
   // Session Replay
   replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
